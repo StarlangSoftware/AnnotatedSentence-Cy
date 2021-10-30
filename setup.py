63365b4c3dccea2cfa84466d3a6bd961d07cb6a6
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 22:07:41 +0300

    Updated setup.py and dictionary.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["AnnotatedSentence/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-AnnotatedSentence-Cy',
-    version='1.0.16',
+    version='1.0.17',
     packages=['AnnotatedSentence'],
     package_data={'AnnotatedSentence': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/AnnotatedSentence-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Annotated Sentence Processing Library',
-    install_requires=['NlpToolkit-WordNet-Cy', 'NlpToolkit-NamedEntityRecognition-Cy', 'NlpToolkit-PropBank-Cy', 'NlpToolkit-DependencyParser-Cy', 'NlpToolkit-SentiNet-Cy']
+    install_requires=['NlpToolkit-WordNet-Cy', 'NlpToolkit-NamedEntityRecognition-Cy', 'NlpToolkit-PropBank-Cy', 'NlpToolkit-DependencyParser-Cy', 'NlpToolkit-SentiNet-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
